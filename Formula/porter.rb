4165ea4e8cc0e00c63503e97807bef8ddd2c1332
Derek Ellis <derekellis170@gmail.com>
2024-03-02 14:39:44 -0500

    Initial Commit.

---
+++ Formula/porter.rb
@@ -0,0 +1,29 @@
+class Porter < Formula
+  desc "Command line tool for syncing arbitrary files."
+  homepage "https://github.com/dellisd/porter"
+  version "latest"
+  head "https://github.com/dellisd/porter.git"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/dellisd/porter/releases/download/latest/porter-macosArm64.zip"
+    else
+      odie "X64 is not currently supported."
+    end
+  else
+    if Hardware::CPU.intel?
+      url "https://github.com/dellisd/porter/releases/download/latest/porter-linuxX64.zip"
+    else
+      odie "ARM is not currently supported."
+    end
+  end
+
+  def install
+    bin.install "porter.kexe" => "porter"
+  end
+
+  test do
+    output = shell_output("#{bin}/porter --help")
+    assert_includes output, "Usage: porter"
+  end
+end
